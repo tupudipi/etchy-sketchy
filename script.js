0db398005648ab188775ddcfc589e234fbe7721b
tupudipi <tupudipi@gmail.com>
2021-10-26 11:45:25 +0300

    added color selection - temporarily not functional

--- script.js
+++ script.js
@@ -2,42 +2,65 @@ let slider = document.getElementById("myRange");
 let output = document.getElementById("demo");
 output.innerText = slider.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
   output.innerText = this.value;
-}
+};
 
 let container = document.createElement("div");
-container.className = 'container';
+container.className = "container";
 
 document.getElementsByClassName("playArea")[0].appendChild(container);
 
 let gridSize = 16;
 
-function draw(e){
-    e.target.classList.add('drawn');
+let drawModeForm = document.getElementById("drawModeForm");
+let drawModeRadios = drawModeForm.elements["drawMode"];
+
+for (let i = 0; i < drawModeRadios.length; i++) {
+  drawModeRadios[i].addEventListener("click", selectDrawMode);
+}
+
+let drawMode = "";
+
+function selectDrawMode() {
+  drawMode = this.id;
+  console.log(drawMode);
+  container.innerHTML = "";
+  divGrid(slider.value);
+}
+
+function draw(e) {
+  switch (drawMode) {
+    case "black":
+      e.target.classList.add("drawn");
+      break;
+    case "greyScale":
+        e.target.classList.add("drawnGrey");
+      break;
+    case "rainbow":
+      e.target.classList.add("drawn");
+      break;
+  }
 }
 
 let divGrid = (gridSize) => {
-                for(let i = 0; i<gridSize; i++)
-                    for(let j = 0; j<gridSize; j++){
-                        let smallDiv = document.createElement("div");
-                        smallDiv.className = 'gridDot';
-                        let size = (720/gridSize);
-                        console.log(size);
-                        size = size.toString() + "px";
-                        smallDiv.style.height = smallDiv.style.width = size;
-                        smallDiv.addEventListener('mouseover', draw);
-                        container.appendChild(smallDiv);
-                    }
-                }
+  container.style.gridTemplateColumns = "repeat(" + gridSize + ", 1fr)";
+  container.style.gridTemplateRows = "repeat(" + gridSize + ", 1fr)";
+  for (let i = 0; i < gridSize; i++)
+    for (let j = 0; j < gridSize; j++) {
+      let smallDiv = document.createElement("div");
+      smallDiv.className = "gridDot";
+      smallDiv.addEventListener("mouseover", draw);
+      container.appendChild(smallDiv);
+    }
+};
 
 let sizeButton = document.getElementById("sizeSelection");
-sizeButton.addEventListener('click', clearAndResize);
+sizeButton.addEventListener("click", clearAndResize);
 
-function clearAndResize(){
-    container.innerHTML = "";
-    divGrid(slider.value);
+function clearAndResize() {
+  container.innerHTML = "";
+  divGrid(slider.value);
 }
-                
+
 divGrid(gridSize);
-                    
